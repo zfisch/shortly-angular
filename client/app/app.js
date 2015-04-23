a0d612a94c47a6eae21e7f6e0711f69684affbf3
zfisch <zacharyrobert@gmail.com>
2015-04-22 19:18:26 -0700

    set up default route and links between links and shorten views

--- client/app/app.js
+++ client/app/app.js
@@ -22,6 +22,9 @@ angular.module('shortly', [
     .when('/shorten' ,{
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
+    })
+    .otherwise({
+      redirectTo: '/links'
     });
 
 
